02505c8e62e1c2a48086b8b83f41e589c5f60777
theunixdisaster <pranavsdreams@gmail.com>
2020-08-02 16:48:33 +0530

    Added the time module which I had forgotten to insert

--- speech.py
+++ speech.py
@@ -2,7 +2,7 @@ import time
 import pyttsx3
 
 #ctime gives time in the format 'Day Month Date hh:mm:ss year'
-fullTime = ctime().split()[3] #This selects the hh:mm:ss
+fullTime = time.ctime().split()[3] #This selects the hh:mm:ss
 fullTime = fullTime[:5] #This selects the hh:mm
 
 text = "The time is " + fullTime #This is our line to speak out
