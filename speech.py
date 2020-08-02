ffa1fa4d83c92c83bf7396ac866d53af184a490e
theunixdisaster <pranavsdreams@gmail.com>
2020-08-02 16:44:52 +0530

    The code now speaks

--- speech.py
+++ speech.py
@@ -4,3 +4,10 @@ import pyttsx3
 #ctime gives time in the format 'Day Month Date hh:mm:ss year'
 fullTime = time.split()[3] #This selects the hh:mm:ss
 fullTime = fullTime[:5] #This selects the hh:mm
+
+text = "The time is " + fullTime #This is our line to speak out
+
+engine = pyttsx3.init() #Initialises our tts engine
+engine.setProperty('rate', 150) #sets the speed of the speech
+engine.say(text)
+engine.runAndWait() #This calls out the time
