0e4a5a722441e0ee36b18e759a123d7da8403a7b
theunixdisaster <pranavsdreams@gmail.com>
2020-08-02 16:46:52 +0530

    Fixed a typo

--- speech.py
+++ speech.py
@@ -2,7 +2,7 @@ import time
 import pyttsx3
 
 #ctime gives time in the format 'Day Month Date hh:mm:ss year'
-fullTime = time.split()[3] #This selects the hh:mm:ss
+fullTime = ctime().split()[3] #This selects the hh:mm:ss
 fullTime = fullTime[:5] #This selects the hh:mm
 
 text = "The time is " + fullTime #This is our line to speak out
